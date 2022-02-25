15f84dfea30914635216372d788fc42b8131887f
Rebecca Hellström Karlsson <rebecca.hellstrom@gmail.com>
2022-02-25 20:57:02 +0100

    Initial commit - setting up a basic restful service

---
+++ src/main/java/com/example/restService/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.restService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+// Class for: controlling (handling) HTTP requests
+@RestController                                                  // Shorthand for using @Controller and @ResponseBody
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
